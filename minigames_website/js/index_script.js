ccf76c96cc387200d994f84fb93948144e680522
Andreas Pedersen <pedersenandreas45@gmail.com>
2023-12-03 23:52:39 +0100

    tic tac toe nearly finished

---
+++ minigames_website/js/index_script.js
@@ -0,0 +1,5 @@
+function displayComingSoon() {
+    // let text = document.getElementById("display_text");
+    // text.innerHTML = "Coming soon!"
+    alert("Coming soon!")
+}
--- minigames_website/js/tictactoe_script.js
+++ minigames_website/js/tictactoe_script.js
@@ -3,10 +3,8 @@ function initGame() {
     let menu = document.getElementById("menu");
     let choose_player = document.getElementById("choose_player");
     menu.style.display = "none";
-    choose_player.style.display = "none"
-    grid.style.display = "flex"
-
-    console.log("Player chose: " + getPlayerChar())
+    choose_player.innerHTML = "YOU PLAY AS " + "<span>" + getPlayerChar() + "</span>";
+    grid.style.display = "flex";
 }
 
 
@@ -20,33 +18,100 @@ function getPlayerChar() {
 
 function getOpponentChar(playerChar) {
     if (playerChar === "X") {
-        playerChar = "O"
+        playerChar = "O";
     } else if (playerChar === "O") {
-        playerChar = "X"
+        playerChar = "X";
     }
 
-    return playerChar
+    return playerChar;
 }
 
 
+// Function placing the bricks, both player and computer
 function drawPlayerChar(field, playerChar) {
     if (field.innerHTML === "") {
-        let empty_field = getField();
-        let opposite_char = getOpponentChar(playerChar);
         field.innerHTML = "<p>" + playerChar + "</p>";
-        empty_field.innerHTML = "<p>" + opposite_char + "</p>";
+        let emptyField = getEmptyField();
+        if (emptyField === null) {
+            gameOver();
+        } else {
+            let opponentChar = getOpponentChar(playerChar);
+            emptyField.innerHTML = "<p>" + opponentChar + "</p>";
+        }
+    }
+    checkFieldsForWin();
+}
+
+
+// Function for finding an unoccupied field
+function getEmptyField() {
+    let fields = document.getElementsByClassName("field");
+    let emptyFields = Array.from(fields).filter(field => field.innerHTML === "");
+
+    if (emptyFields.length === 0) {
+        return null;
+    }
+
+    let randomIndex = Math.floor(Math.random() * emptyFields.length);
+
+    return emptyFields[randomIndex];
+}
+
+
+function checkFieldsForWin() {
+    let fields = document.getElementsByClassName("field");
+    let filledFields = Array.from(fields).filter(field => field.innerHTML != "");
+    let playerChar = getPlayerChar();
+    let opponentChar = getOpponentChar(playerChar);
+
+    // Temporary solution
+    if (filledFields[0].innerHTML === "<p>" + playerChar + "</p>" && filledFields[1].innerHTML === "<p>" + playerChar + "</p>" && filledFields[2].innerHTML === "<p>" + playerChar + "</p>"
+        || filledFields[0].innerHTML === "<p>" + opponentChar + "</p>" && filledFields[1].innerHTML === "<p>" + opponentChar + "</p>" && filledFields[2].innerHTML === "<p>" + opponentChar + "</p>") {
+        return gameWin();
+    }
+    if (filledFields[3].innerHTML === "<p>" + playerChar + "</p>" && filledFields[4].innerHTML === "<p>" + playerChar + "</p>" && filledFields[5].innerHTML === "<p>" + playerChar + "</p>"
+        || filledFields[3].innerHTML === "<p>" + opponentChar + "</p>" && filledFields[4].innerHTML === "<p>" + opponentChar + "</p>" && filledFields[5].innerHTML === "<p>" + opponentChar + "</p>") {
+        return gameWin();
+    }
+    if (filledFields[6].innerHTML === "<p>" + playerChar + "</p>" && filledFields[7].innerHTML === "<p>" + playerChar + "</p>" && filledFields[8].innerHTML === "<p>" + playerChar + "</p>"
+        || filledFields[6].innerHTML === "<p>" + opponentChar + "</p>" && filledFields[7].innerHTML === "<p>" + opponentChar + "</p>" && filledFields[8].innerHTML === "<p>" + opponentChar + "</p>") {
+        return gameWin();
+    }
+    if (filledFields[0].innerHTML === "<p>" + playerChar + "</p>" && filledFields[3].innerHTML === "<p>" + playerChar + "</p>" && filledFields[6].innerHTML === "<p>" + playerChar + "</p>"
+        || filledFields[0].innerHTML === "<p>" + opponentChar + "</p>" && filledFields[3].innerHTML === "<p>" + opponentChar + "</p>" && filledFields[6].innerHTML === "<p>" + opponentChar + "</p>") {
+        return gameWin();
+    }
+    if (filledFields[1].innerHTML === "<p>" + playerChar + "</p>" && filledFields[4].innerHTML === "<p>" + playerChar + "</p>" && filledFields[7].innerHTML === "<p>" + playerChar + "</p>"
+        || filledFields[1].innerHTML === "<p>" + opponentChar + "</p>" && filledFields[4].innerHTML === "<p>" + opponentChar + "</p>" && filledFields[7].innerHTML === "<p>" + opponentChar + "</p>") {
+        return gameWin();
+    }
+    if (filledFields[2].innerHTML === "<p>" + playerChar + "</p>" && filledFields[5].innerHTML === "<p>" + playerChar + "</p>" && filledFields[8].innerHTML === "<p>" + playerChar + "</p>"
+        || filledFields[2].innerHTML === "<p>" + opponentChar + "</p>" && filledFields[5].innerHTML === "<p>" + opponentChar + "</p>" && filledFields[8].innerHTML === "<p>" + opponentChar + "</p>") {
+        return gameWin();
+    }
+    if (filledFields[0].innerHTML === "<p>" + playerChar + "</p>" && filledFields[4].innerHTML === "<p>" + playerChar + "</p>" && filledFields[8].innerHTML === "<p>" + playerChar + "</p>"
+        || filledFields[0].innerHTML === "<p>" + opponentChar + "</p>" && filledFields[4].innerHTML === "<p>" + opponentChar + "</p>" && filledFields[8].innerHTML === "<p>" + opponentChar + "</p>") {
+        return gameWin();
+    }
+    if (filledFields[2].innerHTML === "<p>" + playerChar + "</p>" && filledFields[4].innerHTML === "<p>" + playerChar + "</p>" && filledFields[6].innerHTML === "<p>" + playerChar + "</p>"
+        || filledFields[2].innerHTML === "<p>" + opponentChar + "</p>" && filledFields[4].innerHTML === "<p>" + opponentChar + "</p>" && filledFields[6].innerHTML === "<p>" + opponentChar + "</p>") {
+        return gameWin();
+    }
+    else {
+        return null;
     }
 }
 
 
-function getField() {
-    let fields = document.getElementsByClassName("field");
-    let randomIndex = Math.floor(Math.random() * 9);
-    let randomField = fields[randomIndex];
+function gameOver() {
+    let choose_player = document.getElementById("choose_player");
+    choose_player.innerHTML = "GAME OVER";
+    choose_player.style.color = "#BC4749";
+}
 
-    if (randomField.innerHTML === "") {
-        return randomField;
-    } else {
-        return getField();
-    }
+
+function gameWin() {
+    let choose_player = document.getElementById("choose_player");
+    choose_player.innerHTML = "PLAYER " + "<span>" + getPlayerChar() + "</span>" + " WINS!";
+    choose_player.style.color = "#70E000";
 }
